4d37384820eee60315c64e2adc8f81963742e7fe
ryoiwahara <52648669+ryoiwahara@users.noreply.github.com>
2021-01-15 23:07:11 +0900

    First Commit
    First Commit

---
+++ next_app/pages/index.js
@@ -0,0 +1,20 @@
+export default () => <div>
+    <style jsx>{`
+    h1 {
+        font-size:60px;
+        font-weight: bold;
+        text-align:right;
+        letter-spacing:-8px;
+        color:#f0f0f0;
+        margin: -32px 0;
+    }
+    p{
+        margin:0;
+        color:#666;
+        font-size:16px;
+    }
+    `}</style>
+
+    <h1>Next.js</h1>
+    <div>Welcome to next.js</div>
+</div>
